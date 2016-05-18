db51b18b77a8f0fb54a9620446e1c03e9b2f1017
Mike Perham <mperham@gmail.com>
2016-05-18 13:59:15 -0700

    Work on Redis connections

--- spec/sidekiq_spec.cr
+++ spec/sidekiq_spec.cr
@@ -1,9 +1,43 @@
 require "./spec_helper"
 
 describe Sidekiq do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  describe "basics" do
+    it "has a version" do
+      Sidekiq::VERSION.should_not be_nil
+    end
   end
+
+  describe "pool" do
+    it "works" do
+      t = Time.now
+      pool = ConnectionPool.new { Redis.new }
+      pool.connection do |conn|
+        conn.set("foo", t)
+      end
+      result = pool.connection do |conn|
+        conn.get("foo")
+      end
+      result.should eq(t.to_s)
+    end
+  end
+
+  describe "redis pooling" do
+    it "works" do
+      puts `redis-cli flushall`
+      r = Sidekiq::Connection.new
+      r.with do |conn|
+        conn.get("mike")
+        conn.set("mike", "bob")
+        conn.get("mike")
+      end
+
+      p(r.with do |conn|
+        conn.multi do |multi|
+          multi.get("mike")
+          multi.get("bob")
+        end
+      end)
+    end
+  end
+
 end
--- src/sidekiq.cr
+++ src/sidekiq.cr
@@ -1,5 +1,19 @@
+require "./pool/connection"
 require "./sidekiq/*"
 
+require "redis"
+
 module Sidekiq
-  # TODO Put your code here
+  class Connection
+    # Set up Sidekiq to use the given ConnectionPool to connect to Redis
+    #
+    #     Sidekiq::Redis.new(ConnectionPool.new(capacity: 5, timeout: 5) { Redis.new("localhost", 6379) }
+    #
+    def initialize(@pool = ConnectionPool(Redis).new { Redis.new })
+    end
+
+    def with(&block : Redis -> Redis::RedisValue)
+      @pool.connection(&block)
+    end
+  end
 end
