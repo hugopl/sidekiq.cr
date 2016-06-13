ac6a30a2b07ed4372cbcead8e27703210d2c2937
Mike Perham <mperham@gmail.com>
2016-06-13 11:17:28 -0700

    first pass at JSON serialization layer, #19

--- spec/job_spec.cr
+++ spec/job_spec.cr
@@ -25,8 +25,21 @@ describe Sidekiq::Job do
 
         results.size.should eq(1)
         str = results[0].as(String)
-        hash = JSON.parse(str)
+        hash = JSON.parse(str).as_h
         hash.size.should eq(11)
+
+        job = Sidekiq::Job.new
+        job.load(hash)
+        aft = job.to_h
+        hash.keys.each do |key|
+          if key =~ /at/
+            hf = hash[key].as(Float64)
+            af = aft[key].as(Float64)
+            (hf * 1000).to_i64.should eq((af * 1000).to_i64)
+          else
+            hash[key].should eq(aft[key])
+          end
+        end
       end
     end
   end
--- src/sidekiq/job.cr
+++ src/sidekiq/job.cr
@@ -3,9 +3,13 @@ require "./core_ext"
 
 module Sidekiq
 
+  ##
+  # The Job class handles the internal business of converting
+  # to/from JSON.  In a statically-typed language, this is
+  # a bit of a chore.
   class Job
     #
-    # This global registration is a bit of a bloody hack.
+    # This global registration is a bloody hack.
     # Unclear of a better way of doing it.
     #
     @@jobtypes = Hash(String, -> Sidekiq::Worker).new
@@ -18,25 +22,51 @@ module Sidekiq
       @@jobtypes[name] = klass
     end
 
-    JSON.mapping({
-      queue: String,
-      args: Array(JSON::Type),
-      klass: { key: "class", type: String },
-      created_at: {type: Time, converter: EpochConverter},
-      enqueued_at: {type: Time, converter: EpochConverter, nilable: true},
-      jid: String,
-      at: { type: Time, converter: EpochConverter, nilable: true },
-      bid: { type: String, nilable: true },
-      retry: { type: JSON::Type, nilable: true }, # (Bool, Int64, Nil)
-      retry_count: { type: Int64, nilable: true },
-      backtrace: { type: JSON::Type, nilable: true }, # (Bool, Int64, Nil)
-      dead: { type: Bool, nilable: true },
-      failed_at: {type: Time, converter: EpochConverter, nilable: true},
-      retried_at: {type: Time, converter: EpochConverter, nilable: true},
-      error_class: { type: String, nilable: true },
-      error_message: { type: String, nilable: true },
-      error_backtrace: { type: Array(String), nilable: true },
-    })
+    property! queue : String
+    property! jid : String
+    property! klass : String
+    property! args : Array(JSON::Type)
+    property! created_at : Time
+
+    property bid : String?
+    property at : Time?
+    property failed_at : Time?
+    property retried_at : Time?
+    property enqueued_at : Time?
+    property retry_count : Int64?
+    property error_class : String?
+    property error_message : String?
+    property error_backtrace : Array(String)?
+    property dead : Bool?
+    property backtrace : (Bool | Int64 | Nil)
+    property retry : (Bool | Int64 | Nil)
+
+    def to_h : Hash(String, JSON::Type)
+      h = Hash(String, JSON::Type).new
+      h["jid"] = jid
+      h["args"] = args
+      h["queue"] = queue
+      h["class"] = klass
+      h["created_at"] = created_at.epoch_f
+
+      h["bid"] = bid if bid
+      h["at"] = at.not_nil!.epoch_f if at
+      h["failed_at"] = failed_at.not_nil!.epoch_f if failed_at
+      h["retried_at"] = retried_at.not_nil!.epoch_f if retried_at
+      h["enqueued_at"] = enqueued_at.not_nil!.epoch_f if enqueued_at
+      h["retry_count"] = retry_count if retry_count
+      h["dead"] = dead if dead
+      h["backtrace"] = backtrace if backtrace
+      h["retry"] = retry if retry
+      h["error_class"] = error_class if error_class
+      h["error_message"] = error_message if error_message
+      h["error_backtrace"] = error_backtrace.not_nil!.map{|x| x.as(JSON::Type)} if error_backtrace
+      h
+    end
+
+    def to_json : String
+      to_h.to_json
+    end
 
     def initialize
       @queue = "default"
@@ -56,8 +86,29 @@ module Sidekiq
       self.bid = hash["bid"]?.try &.to_s
       self.error_class = hash["error_class"]?.try &.to_s
       self.error_message = hash["error_message"]?.try &.to_s
-      self.backtrace = hash["backtrace"]?
-      self.retry = hash["retry"]?
+
+      x = hash["backtrace"]?
+      if x && x.is_a?(Int64)
+        self.backtrace = x.as(Int64)
+      elsif x && x.is_a?(Bool)
+        self.backtrace = x.as(Bool)
+      elsif x
+        raise "Invalid 'backtrace' value: #{x.inspect} / #{x.class.name}"
+      else
+        self.backtrace = nil
+      end
+
+      x = hash["retry"]?
+      if x && x.is_a?(Int64)
+        self.retry = x.as(Int64)
+      elsif x && x.is_a?(Bool)
+        self.retry = x.as(Bool)
+      elsif x
+        raise "Invalid 'retry' value: #{x.inspect} / #{x.class.name}"
+      else
+        self.retry = nil
+      end
+
       self.retry_count = hash["retry_count"]?.try &.as(Int64)
       self.dead = hash["dead"]?.try &.as(Bool)
       if hash["at"]?
@@ -79,6 +130,11 @@ module Sidekiq
       if hash["failed_at"]?
         x = hash["failed_at"].as(Float64)
         self.failed_at = Time.epoch_ms((x * 1000).to_i64)
+      end
+
+      x = hash["error_backtrace"]?
+      if x && x.is_a?(Array)
+        self.error_backtrace = x.as(Array).map{|y| y.as(String) }
       end
     end
 
