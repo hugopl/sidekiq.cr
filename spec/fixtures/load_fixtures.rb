01e0eab82c5ccdf2d1947765ff653a3f25e7fb52
Mike Perham <mperham@gmail.com>
2016-06-13 10:39:30 -0700

    #18

--- spec/fixtures/load_fixtures.rb
+++ spec/fixtures/load_fixtures.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 
-require "sidekiq"
+canretry = true
+begin
+  require "redis"
+rescue
+  raise unless canretry
+  puts `gem install redis`
+  canretry = false
+  retry
+end
 
 filename = ARGV[0]
 puts "Loading fixture #{filename}"
@@ -8,6 +16,7 @@ hash = File.open(filename, "rb") do |file|
   Marshal.load(file.read)
 end
 
+r = Redis.new
 hash.each_pair do |key, value|
-  Sidekiq.redis {|c| c.restore(key, 1000, value) }
+  r.restore(key, 1000, value)
 end
