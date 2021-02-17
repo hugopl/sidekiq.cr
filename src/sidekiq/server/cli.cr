7422fc57a95f8fe23dd4e032c9ae416ad68ce985
Holden Omans <holden.omans@gmail.com>
2021-02-17 13:33:42 -0500

    Update src/sidekiq/server/cli.cr
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/sidekiq/server/cli.cr
+++ src/sidekiq/server/cli.cr
@@ -5,7 +5,7 @@ require "../server"
 
 module Sidekiq
   class CLI
-    getter logger : ::Log = ::Log.for(Sidekiq::CLI)
+    getter logger : ::Log = ::Log.for(self)
 
     def initialize(args = ARGV)
       @concurrency = 25
